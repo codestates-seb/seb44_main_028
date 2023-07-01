0f3bf4158d43f9eace48443a11924b93dd59ad87
MinHwan <108858620+minit88@users.noreply.github.com>
2023-07-01 19:51:10 +0900

    Update deploy.sh

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -26,4 +26,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-sudo nohup java -jar $BUILD_JAR >> /home/ubuntu/action/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/action/deploy.log 2>/home/ubuntu/action/deploy_err.log &
