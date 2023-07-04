bd335217500ae2a39bf9bcc858f21bf7fa122257
parkchaeyeon <pcy6904@naver.com>
2023-07-05 03:33:35 +0900

    🔥 불필요한 코드 삭제

--- client/src/pages/Header/views/Header.tsx
+++ client/src/pages/Header/views/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { MdSearch, MdSend, MdLogout } from 'react-icons/md';
 import { LogoText, NavMenu } from '../constants';
 import {
