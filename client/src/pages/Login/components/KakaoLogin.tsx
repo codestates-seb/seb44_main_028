e1344c289a7052b2064708f48ef89515779a8237
jasmine0714 <woooooooow22@gmail.com>
2023-07-05 07:41:43 +0900

    ✨ 유저를 redirection uri로 리디렉트 후 url에 있는 인가코드를 상태에 저장

--- client/src/pages/Login/components/KakaoLogin.tsx
+++ client/src/pages/Login/components/KakaoLogin.tsx
@@ -1,6 +1,35 @@
+import axios from 'axios';
 import { KakaoLoginBtn } from '../style';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 function KakaoLogin() {
-  return <KakaoLoginBtn />;
+  const location = useLocation();
+  const [authorizationCode, setAuthorizationCode] = useState('');
+  const handleKakaoLogin = async () => {
+    try {
+      const response = await axios.get('https://playpack.shop/api/members/me');
+      if (response.status === 200) {
+        window.location.href = response.data.uri;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getAuthorizationCode = () => {
+    const authorizationCode = new URLSearchParams(location.search).get('code');
+    if (authorizationCode) {
+      console.log('인가 코드', authorizationCode);
+      setAuthorizationCode(authorizationCode);
+    } else {
+      console.log('no authorizationCode');
+    }
+  };
+
+  useEffect(() => {
+    getAuthorizationCode();
+  }, [location]);
+  return <KakaoLoginBtn onClick={handleKakaoLogin} />;
 }
 export default KakaoLogin;
