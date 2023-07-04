dd3cd0266d93bfc8f02713cf2d1950dd036b139e
jasmine0714 <woooooooow22@gmail.com>
2023-07-05 00:17:53 +0900

    🏷️ style enum 생성(colorPalette, fontSize, border)

---
+++ client/src/common/utils/enum/colorPalette.ts
@@ -0,0 +1,9 @@
+export enum colorPalette {
+  lightColor = '#B0F4E6',
+  accentColor = '#12D3CF',
+  heavyColor = '#OD4C92',
+  basicTextColor = '#333',
+  grayTextColor = '#777',
+  tagColor = '#74D4E4',
+  modalIconColor = '#D0E2Ef',
+}
