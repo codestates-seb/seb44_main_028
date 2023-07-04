83d374d667debe2f697fe085f72345492dc0f30d
jasmine0714 <woooooooow22@gmail.com>
2023-07-05 08:14:05 +0900

    ⚡️ 리디렉션된 url에서 인가 코드를 추출하여 유저정보 get 요청

--- client/src/pages/Login/components/KakaoLogin.tsx
+++ client/src/pages/Login/components/KakaoLogin.tsx
@@ -4,9 +4,31 @@ import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { KakaoLoginBtn } from '../style';
 
+const fetchUserData = async (code: string | null) => {
+  if (!code) return;
+  const { data } = await axios.get(
+    `https://playpack.shop/api/members/me?code=${code}`,
+  );
+  return data;
+};
+
 function KakaoLogin() {
   const location = useLocation();
-  const [authorizationCode, setAuthorizationCode] = useState('');
+  const [authorizationCode, setAuthorizationCode] = useState<string | null>('');
+
+  const {
+    data: userData,
+    isError,
+    error,
+  } = useQuery(
+    ['user', authorizationCode],
+    () => fetchUserData(authorizationCode),
+    {
+      enabled: !!authorizationCode,
+    },
+  );
+  console.log(userData);
+
   const handleKakaoLogin = async () => {
     try {
       const response = await axios.get('https://playpack.shop/api/members/me');
