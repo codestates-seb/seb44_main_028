da862415deacc6903e5cdc900e2d909255ca8a9a
hdwmumu <wil0110@naver.com>
2023-07-04 21:18:55 +0900

    ✨ mypage 기초공사

--- client/src/pages/MyPage/views/MyPage.tsx
+++ client/src/pages/MyPage/views/MyPage.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
-import Page from '../../../components/Page';
-import Header from '../components/Header';
-import Tab from '../components/Tab';
-import Main from '../components/Main';
+import React, { useState, useEffect } from 'react';
 
+type User = {
+  name: string;
+  email: string;
+};
 
-function MyPage() {
+const MyPage: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  // 사용자 정보 가져오기 (임시로 작성된 코드)
+  const fetchUserInfo = () => {
+    // API 호출 또는 데이터베이스에서 사용자 정보를 가져온다고 가정
+    const userInfo: User = {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    };
+    setUser(userInfo);
+  };
+
+  // 마이페이지 컴포넌트가 처음 렌더링될 때 사용자 정보를 가져옴
+  useEffect(() => {
+    fetchUserInfo();
+  }, []);
+
   return (
-    <Page>
-      <Header />
-      <Tab />
-      <Main />
-    </Page>
+    <div>
+      <h2>My Page</h2>
+      {user ? (
+        <div>
+          <p>Name: {user.name}</p>
+          <p>Email: {user.email}</p>
+        </div>
+      ) : (
+        <p>Loading user information...</p>
+      )}
+    </div>
   );
-}
+};
 
 export default MyPage;
