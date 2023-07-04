412b5f76a1683021c2de370d53a6bfbc47838269
minit88 <chymh32@gmail.com>
2023-07-04 18:58:36 +0900

    :sparkles: 배포 테스트

--- server/src/main/java/com/ftiland/travelrental/image/controller/ImageController.java
+++ server/src/main/java/com/ftiland/travelrental/image/controller/ImageController.java
@@ -1,16 +1,18 @@
 package com.ftiland.travelrental.image.controller;
 
+import com.ftiland.travelrental.image.entity.Image;
 import com.ftiland.travelrental.image.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController("/")
-public class imageController {
+public class ImageController {
     private FileStorageService fileStorageService;
 
     @Autowired
@@ -19,9 +21,17 @@ public class imageController {
     }
 
     @PostMapping("image")
-    public ResponseEntity example01(@RequestParam("imageFile")MultipartFile imageFile){
-        String url = fileStorageService.storeFile(imageFile);
+    public ResponseEntity postImage(@RequestParam("imageFile")MultipartFile imageFile){
 
-        return new ResponseEntity(url, HttpStatus.OK);
+        Image image = fileStorageService.storeImageProduct(imageFile);
+
+        return new ResponseEntity(image, HttpStatus.OK);
     }
+    @GetMapping("iamge")
+    public ResponseEntity postImage2(){
+
+
+    return new ResponseEntity("dasd", HttpStatus.OK);
+    }
+
 }
