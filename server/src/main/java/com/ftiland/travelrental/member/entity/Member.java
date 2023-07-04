7f1b0f17e9b76a4d48b61b8e01e10191aaf2a3a4
ahyoung227 <ahyoung227@gmail.com>
2023-07-05 08:49:14 +0900

    :sparkles: 카카오 로그인시 멤버 객체 생성을 위해 repository, service 객체 생성

--- server/src/main/java/com/ftiland/travelrental/member/entity/Member.java
+++ server/src/main/java/com/ftiland/travelrental/member/entity/Member.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
@@ -15,8 +17,9 @@ import javax.persistence.Id;
 @Entity
 public class Member extends BaseEntity {
 
-    @Id // UUID사용
-    private String memberId;
+    @Id // 멤버의 경우 숫자형식의 id 사용
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
     private String email;
     private String name;
 
@@ -24,6 +27,10 @@ public class Member extends BaseEntity {
 
     private Double latitude;
     private Double longitude;
+
+    public Member(String email) {
+        this.email = email;
+    }
 
 /*    private Double totalRateScore;
     private Double totalRateCount;*/
