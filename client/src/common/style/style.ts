b88aab0ffec7bad56ec3fe6c494d28afaca203a0
parkchaeyeon <pcy6904@naver.com>
2023-07-05 15:36:08 +0900

    🔥 불필요한 코드 삭제

--- client/src/common/style/style.ts
+++ client/src/common/style/style.ts
@@ -1,4 +1,4 @@
-import styled, { css, keyframes } from 'styled-components';
+import styled from 'styled-components';
 import { colorPalette } from '../utils/enum/colorPalette';
 
 export const ItemCardContainer = styled.div`
