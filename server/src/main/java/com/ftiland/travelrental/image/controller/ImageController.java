f2e4c6423e719c2b12edcb2386a9450f4b93adcf
minit88 <chymh32@gmail.com>
2023-07-05 11:15:14 +0900

    :sparkles: 이미지 컨트롤러 수정

--- server/src/main/java/com/ftiland/travelrental/image/controller/ImageController.java
+++ server/src/main/java/com/ftiland/travelrental/image/controller/ImageController.java
@@ -5,10 +5,7 @@ import com.ftiland.travelrental.image.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController("/")
@@ -33,5 +30,6 @@ public class ImageController {
 
     return new ResponseEntity("dasd", HttpStatus.OK);
     }
+
 
 }
