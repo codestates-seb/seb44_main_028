005e1f6bf5f6c2a75189ee637727699b9706e468
parkchaeyeon <pcy6904@naver.com>
2023-07-06 17:04:37 +0900

    🔨 select option 데이터 생성

---
+++ client/src/common/constants.ts
@@ -0,0 +1,32 @@
+export const DISTANCE_OPTIONS = [
+  {
+    value: 10,
+    label: '10k㎡',
+  },
+  {
+    value: 20,
+    label: '20k㎡',
+  },
+  {
+    value: 30,
+    label: '30k㎡',
+  },
+];
+export const PRODUCT_FILTER_OPTIONS = [
+  {
+    value: 'new',
+    label: '최신순',
+  },
+  {
+    value: 'starRating',
+    label: '별점순',
+  },
+  {
+    value: 'view',
+    label: '조회순',
+  },
+  {
+    value: 'nearest',
+    label: '가까운순',
+  },
+];
