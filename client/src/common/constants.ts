bb89da5c3b950f008346717f7097c04d2cda0e23
parkchaeyeon <pcy6904@naver.com>
2023-07-06 15:08:02 +0900

    🔨 카테고리 리스트 데이터 추가

---
+++ client/src/common/constants.ts
@@ -0,0 +1,53 @@
+import climbing from '../../public/assets/category_icon/등산.svg';
+import camping from '../../public/assets/category_icon/캠핑.svg';
+import ski from '../../public/assets/category_icon/스키.svg';
+import waterPlay from '../../public/assets/category_icon/물놀이.svg';
+import forLiving from '../../public/assets/category_icon/생활용품.svg';
+import forShooting from '../../public/assets/category_icon/촬영용품.svg';
+import play from '../../public/assets/category_icon/놀거리.svg';
+import clothes from '../../public/assets/category_icon/의류.svg';
+import accessory from '../../public/assets/category_icon/액서서리.svg';
+import others from '../../public/assets/category_icon/기타.svg';
+
+export const CATEGORY = [
+  {
+    image: climbing,
+    name: '등산',
+  },
+  {
+    image: camping,
+    name: '캠핑',
+  },
+  {
+    image: ski,
+    name: '스키',
+  },
+  {
+    image: waterPlay,
+    name: '물놀이',
+  },
+  {
+    image: forLiving,
+    name: '생활용품',
+  },
+  {
+    image: forShooting,
+    name: '촬영용품',
+  },
+  {
+    image: play,
+    name: '놀거리',
+  },
+  {
+    image: clothes,
+    name: '의류',
+  },
+  {
+    image: accessory,
+    name: '액서서리',
+  },
+  {
+    image: others,
+    name: '기타',
+  },
+];
