decfcdd0323c4ae5eefcc3ecb5f4b32a861a353f
parkchaeyeon <pcy6904@naver.com>
2023-07-06 15:30:32 +0900

    💄 카테고리 리스트 컴포넌트, 스타일

---
+++ client/src/common/components/Category/Category.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import CategoryButton from './CategoryButton';
+import { CATEGORY } from '../../constants';
+import { CategoryContainer } from '../../style/style';
+
+const Category = () => {
+  return (
+    <CategoryContainer>
+      {CATEGORY.map((categoryBtn) => (
+        <CategoryButton
+          imageUrl={categoryBtn.image}
+          imageName={categoryBtn.name}
+        />
+      ))}
+    </CategoryContainer>
+  );
+};
+
+export default Category;
--- client/src/common/style/style.ts
+++ client/src/common/style/style.ts
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 import { fontSize } from '../utils/enum/fontSize';
 
-export const CategoryContainer = styled.div``;
+export const CategoryContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 4.275rem 2.4rem;
+`;
 export const CategoryButtonWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   font-size: ${fontSize.basic};
+  cursor: pointer;
   & img {
     width: 100px;
     height: 100px;
