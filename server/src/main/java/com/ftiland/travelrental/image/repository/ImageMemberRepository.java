1569362c239145798e70d5576d0a2165fcd76451
minit88 <chymh32@gmail.com>
2023-07-06 11:07:47 +0900

    :card_file_box: 레포지 수정

--- server/src/main/java/com/ftiland/travelrental/image/repository/ImageMemberRepository.java
+++ server/src/main/java/com/ftiland/travelrental/image/repository/ImageMemberRepository.java
@@ -1,8 +1,15 @@
 package com.ftiland.travelrental.image.repository;
 
 
+import com.ftiland.travelrental.image.entity.Image;
 import com.ftiland.travelrental.image.entity.ImageMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ImageMemberRepository extends JpaRepository<ImageMember,String> {
+    @Query("SELECT i FROM ImageMember i WHERE i.member.memberId =:memberId")
+    List<ImageMember> findByMemberId(@Param("memberId") Long memberId);
 }
