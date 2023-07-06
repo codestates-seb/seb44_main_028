cfddf3d509204f7b820eded1f6a720a3664d9590
parkchaeyeon <pcy6904@naver.com>
2023-07-06 11:31:40 +0900

    💄 CategoryButton 스타일 추가

--- client/src/common/style/style.ts
+++ client/src/common/style/style.ts
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
+import { fontSize } from '../utils/enum/fontSize';
 
+export const CategoryContainer = styled.div``;
 export const CategoryButtonWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  font-size: ${fontSize.basic};
+  & img {
+    width: 100px;
+    height: 100px;
+    margin-bottom: 20px;
+  }
 `;
