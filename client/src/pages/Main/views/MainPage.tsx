ebe89723251f2db8b0e3c62c51e0518f1ddd275c
parkchaeyeon <pcy6904@naver.com>
2023-07-07 13:49:55 +0900

    🍱 이미지 슬라이더 사진 추가

--- client/src/pages/Main/views/MainPage.tsx
+++ client/src/pages/Main/views/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ScrollToTop from '../../../common/components/ScrollToTop';
+import Carousel from '../components/Carousel';
 
 function MainPage() {
   return (
     <>
       <div>Main page</div>
+      <Carousel />
       <ScrollToTop />
     </>
   );
