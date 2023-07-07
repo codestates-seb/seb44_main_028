4a2cdbf589287d087f53e2dbb4ed0fb0e8777230
parkchaeyeon <pcy6904@naver.com>
2023-07-07 14:04:18 +0900

    🔧 데이터 파일 추가

---
+++ client/src/pages/Main/constants.ts
@@ -0,0 +1,23 @@
+import MainImage from '../../assets/image_slider/MainImage.svg';
+import CampingImage from '../../assets/image_slider/CampingImage.svg';
+import BeachImage from '../../assets/image_slider/BeachImage.svg';
+import SkiImage from '../../assets/image_slider/SkiImage.svg';
+
+export const IMAGE_SLIDER = [
+  {
+    image: MainImage,
+    name: '메인',
+  },
+  {
+    image: CampingImage,
+    name: '캠핑',
+  },
+  {
+    image: BeachImage,
+    name: '바다',
+  },
+  {
+    image: SkiImage,
+    name: '스키',
+  },
+];
