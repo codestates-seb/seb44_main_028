6fabd69ac726ee4f16262232013627dcc0f0b8e8
parkchaeyeon <pcy6904@naver.com>
2023-07-10 01:43:39 +0900

    🏷️ 타입지정

--- client/src/pages/Header/constants.ts
+++ client/src/pages/Header/constants.ts
@@ -1,5 +1,6 @@
+import { NavMenuListData } from './type';
 export const LogoText = '플레이 팩';
-export const NavMenuList = [
+export const NavMenuList: NavMenuListData[] = [
   {
     href: '/mypage',
     title: '마이페이지',
