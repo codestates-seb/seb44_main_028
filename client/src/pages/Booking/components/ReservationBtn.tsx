cd4c0bf247cd296ea37e7b504dfa2f7638b94001
jasmine0714 <woooooooow22@gmail.com>
2023-07-09 10:26:56 +0900

    💄 height, width, children을 prop으로 받는 버튼으로 대체

--- client/src/pages/Booking/components/ReservationBtn.tsx
+++ client/src/pages/Booking/components/ReservationBtn.tsx
@@ -1,4 +1,6 @@
+import BigBtn from '../../../common/components/BigButton';
+
 function ReservationBtn() {
-  return <div>버튼</div>;
+  return <BigBtn height={57} width={175} children={'예약하기'} />;
 }
 export default ReservationBtn;
