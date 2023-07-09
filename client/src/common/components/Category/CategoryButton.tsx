858e7e965eacc32277c1d93b41d6d49ba6660035
parkchaeyeon <pcy6904@naver.com>
2023-07-10 00:26:17 +0900

    🔥 불필요한 코드 제거

--- client/src/common/components/Category/CategoryButton.tsx
+++ client/src/common/components/Category/CategoryButton.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { CategoryButtonProps } from '../../type';
 import { CategoryButtonWrapper } from '../../style/style';
 import { useNavigate } from 'react-router-dom';
--- client/src/pages/Header/views/Header.tsx
+++ client/src/pages/Header/views/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import { MdSearch, MdSend, MdLogout } from 'react-icons/md';
 import { LogoText, NavMenu } from '../constants';
