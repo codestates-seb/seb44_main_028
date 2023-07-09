1cbc4eeea22fc43a9ae71a5eb66b0cd63ef991ac
parkchaeyeon <pcy6904@naver.com>
2023-07-10 04:01:09 +0900

    🧪 테스트 코드 작성

--- client/src/pages/Main/views/MainPage.tsx
+++ client/src/pages/Main/views/MainPage.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import { useState } from 'react';
+import ModalMain from '../../../common/components/Modal/ModalMain';
 import ScrollToTop from '../../../common/components/ScrollToTop';
 
 function MainPage() {
+  const [isClick, setIsClick] = useState(false);
+  const handleModalTest = () => {
+    setIsClick(!isClick);
+  };
   return (
     <>
       <div>Main page</div>
+      <button onClick={handleModalTest}>버튼</button>
+      {isClick && <ModalMain isOpen={isClick} />}
       <ScrollToTop />
     </>
   );
