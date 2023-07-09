ccc6d0b08c14e6a400c48ea9c93e1728e0cdb4e6
parkchaeyeon <pcy6904@naver.com>
2023-07-10 01:39:29 +0900

    🎨 NavMenuList 데이터 수정

--- client/src/pages/Header/constants.ts
+++ client/src/pages/Header/constants.ts
@@ -1,2 +1,11 @@
 export const LogoText = '플레이 팩';
-export const NavMenu: string[] = ['마이페이지', '글 작성하기'];
+export const NavMenuList = [
+  {
+    href: '/mypage',
+    title: '마이페이지',
+  },
+  {
+    href: '/create',
+    title: '글 작성하기',
+  },
+];
