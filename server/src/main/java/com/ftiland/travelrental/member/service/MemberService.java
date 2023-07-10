712b48ce9418a4296e010ee0fe89e2ebf46dcff3
ahyoung227 <ahyoung227@gmail.com>
2023-07-10 09:07:52 +0900

    :sparkles: retrieve redirect logic

--- server/src/main/java/com/ftiland/travelrental/member/service/MemberService.java
+++ server/src/main/java/com/ftiland/travelrental/member/service/MemberService.java
@@ -3,11 +3,18 @@ package com.ftiland.travelrental.member.service;
 
 import com.ftiland.travelrental.common.exception.BusinessLogicException;
 import com.ftiland.travelrental.common.exception.ExceptionCode;
+import com.ftiland.travelrental.member.dto.MemberDto;
+import com.ftiland.travelrental.member.dto.MemberPatchDto;
 import com.ftiland.travelrental.member.entity.Member;
 import com.ftiland.travelrental.member.repository.MemberRepository;
+import com.ftiland.travelrental.product.dto.UpdateProduct;
+import com.ftiland.travelrental.product.entity.Product;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
+import static com.ftiland.travelrental.common.exception.ExceptionCode.MEMBER_NOT_FOUND;
+import static com.ftiland.travelrental.common.exception.ExceptionCode.PRODUCT_NOT_FOUND;
 
 @Service
 public class MemberService {
@@ -41,6 +48,22 @@ public class MemberService {
                     new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND)
                 );
         return findMember;
+    }
+
+    private MemberDto.Response updateMember(MemberPatchDto.Request request, Long memberId) {
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new BusinessLogicException(MEMBER_NOT_FOUND));
+
+        Optional.ofNullable(request.getDisplayName())
+                .ifPresent(displayName -> member.setDisplayName(displayName));
+        Optional.ofNullable(request.getLatitude())
+                .ifPresent(latitude -> member.setLatitude(latitude));
+        Optional.ofNullable(request.getLongitude())
+                .ifPresent(longitude -> member.setLongitude(longitude));
+
+        return MemberDto.Response.from(member);
+
     }
 
 }
