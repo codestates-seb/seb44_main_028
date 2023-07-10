1edb82b2dafeaebda69157dc2ad364691d757e78
parkchaeyeon <pcy6904@naver.com>
2023-07-10 21:13:54 +0900

    💄 ModalWrapper box-shadow 변수 값 지정

--- client/src/common/style/style.ts
+++ client/src/common/style/style.ts
@@ -65,8 +65,7 @@ export const ModalWrapper = styled.div`
   transform: translate(-50%, -50%);
   font-size: ${fontSize.basic};
   z-index: 6;
-  box-shadow: 0 10px 24px hsla(0, 0%, 0%, 0.05),
-    0 20px 48px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.1);
+  box-shadow: ${BoxShadow.Basic};
   & svg {
     font-size: 4rem;
     color: ${colorPalette.modalIconColor};
