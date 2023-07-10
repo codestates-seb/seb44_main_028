da42953ec217df8c0d61abe1908736b15de7270e
minit88 <chymh32@gmail.com>
2023-07-11 00:36:11 +0900

    ✨  배포 문제 해결

--- server/src/test/java/com/ftiland/travelrental/interest/controller/InterestControllerTest.java
+++ server/src/test/java/com/ftiland/travelrental/interest/controller/InterestControllerTest.java
@@ -1,84 +1,69 @@
 package com.ftiland.travelrental.interest.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ftiland.travelrental.category.dto.CategoryDto;
 import com.ftiland.travelrental.interest.dto.InterestDto;
 import com.ftiland.travelrental.interest.entity.Interest;
 import com.ftiland.travelrental.interest.mapper.InterestMapper;
 import com.ftiland.travelrental.interest.service.InterestService;
 import com.ftiland.travelrental.member.entity.Member;
-import com.ftiland.travelrental.product.dto.CreateProduct;
-import com.ftiland.travelrental.product.dto.UpdateProduct;
 import com.ftiland.travelrental.product.entity.Product;
-import com.ftiland.travelrental.product.service.ProductService;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import org.mockito.Mockito;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
-import java.util.List;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.startsWith;
-import static org.mockito.ArgumentMatchers.*;
+import java.util.ArrayList;
+
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+
+@WebMvcTest(InterestMemberController.class)
 public class InterestControllerTest {
     @Autowired
     private MockMvc mockMvc;
-
-
     @MockBean
     private InterestService interestService;
 
     @MockBean
     private InterestMapper interestMapper;
 
-    @MockBean
+    @Autowired
     private ObjectMapper objectMapper;
-/*
-    @MockBean
-    private  InterestContro
 
- */
     @Test
-    @WithMockUser(username = "사용자",roles = {"USER"})
-    void CreateInteterest_SUCCESS() throws Exception{
-        // given
+    @DisplayName("찜하기 생성 컨트롤러 성공")
+    @WithMockUser(username = "사용자", roles = {"USER"})
+    void CreateInterest_SUCCESS() throws Exception{
 
-        Member member = new Member(1L,
-                "test@test.com",
-                "둘리",
-                37.5793493362539,
-                126.91794995956589);
+        // given
+        Member member = Member.builder()
+                .memberId(1L)
+                .email("test@test.com")
+                .displayName("둘리")
+                .latitude(37.5793493362539)
+                .longitude(126.91794995956589).build();
 
         Product product = Product.builder()
                 .productId("91052a17-bca6-4fde-a586-a1d179ad3463")
                 .title("제목입니다.")
                 .content("내용입니다.")
-                .content("내용입니다.")
                 .member(member).build();
-
-        MultiValueMap<String, String> info = new LinkedMultiValueMap<>();
-
-        info.add("memberId", "1L");
-        info.add("productId", "1234");
 
         Interest interest = Interest.builder().interestId("1234").product(product).member(member).build();
 
@@ -87,16 +72,110 @@ public class InterestControllerTest {
         response.setProductId("91052a17-bca6-4fde-a586-a1d179ad3463");
         response.setMemberId(1L);
 
-        given(interestService.createInterest(Mockito.anyLong(), Mockito.anyString())).willReturn(interest);
+
+
+        given(interestService.createInterest(Mockito.anyLong(),Mockito.anyString())).willReturn(interest);
+        given(interestMapper.interestToResponseDto(Mockito.any())).willReturn(response);
 
         // when
-        ResultActions actions = mockMvc.perform(
-                    post("/api/members/interests")
-                    .params(info))
-                    .andExpect(status().isCreated());
+        ResultActions postActions = mockMvc.perform(
+                post("/api/members/interests")
+                        .param("memberId",member.getMemberId().toString())
+                        .param("productId",product.getProductId().toString())
+                        .with(csrf()));
 
         // then
-        actions.andExpect(status().isCreated()).andExpect(header().string("Location",is(startsWith("/api/members/"))));
+        postActions
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("memberId").value(member.getMemberId()))
+                .andExpect(jsonPath("productId").value(product.getProductId()));
+    }
+
+    @Test
+    @DisplayName("찜하기 해제 컨트롤러 성공")
+    @WithMockUser(username = "사용자", roles = {"USER"})
+    void DeleteInterest_Success() throws Exception{
+        // given
+        Member member = Member.builder()
+                .memberId(1L)
+                .email("test@test.com")
+                .displayName("둘리")
+                .latitude(37.5793493362539)
+                .longitude(126.91794995956589).build();
+
+        Product product = Product.builder()
+                .productId("91052a17-bca6-4fde-a586-a1d179ad3463")
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .member(member).build();
+
+        Interest interest = Interest.builder().interestId("1234").product(product).member(member).build();
+
+        InterestDto.ResponseDto response = new InterestDto.ResponseDto();
+        response.setInterestId("1234");
+        response.setProductId("91052a17-bca6-4fde-a586-a1d179ad3463");
+        response.setMemberId(1L);
+
+        // when
+        ResultActions postActions = mockMvc.perform(
+                delete("/api/members/interests")
+                        .param("memberId",member.getMemberId().toString())
+                        .param("interestId",interest.getInterestId().toString())
+                        .with(csrf()));
+
+        // then
+        postActions
+                .andExpect(status().isOk());
 
     }
+    @Test
+    @DisplayName("찜목록 조회 컨트롤러 성공")
+    @WithMockUser(username = "사용자", roles = {"USER"})
+    void GetInterest_Success()throws Exception{
+
+        // given
+        Member member = Member.builder()
+                .memberId(1L)
+                .email("test@test.com")
+                .displayName("둘리")
+                .latitude(37.5793493362539)
+                .longitude(126.91794995956589).build();
+
+        Product product = Product.builder()
+                .productId("91052a17-bca6-4fde-a586-a1d179ad3463")
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .member(member).build();
+        Product product2 = Product.builder()
+                .productId("91052a17-bca6-4fde-a586-a1d179ad34634")
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .member(member).build();
+        Interest interest = Interest.builder().interestId("1234").product(product).member(member).build();
+        Interest interest2 = Interest.builder().interestId("12345").product(product2).member(member).build();
+
+        ArrayList<Interest> interests = new ArrayList<>();
+        interests.add(interest2);
+        interests.add(interest);
+        InterestDto.ResponsesDto responses = interestMapper.interestsToResponsesDto(interests);
+
+        int size = interests.size();
+        given(interestService.findInterest(Mockito.anyLong())).willReturn(interests);
+        given(interestMapper.interestsToResponsesDto(Mockito.any(ArrayList.class))).willReturn(responses);
+
+
+        // when
+        ResultActions getActions = mockMvc.perform(
+                get("/api/members/interests")
+                        .param("memberId",member.getMemberId().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf()));
+
+        // then
+        getActions
+                .andExpect(status().isOk());
+
+    }
+
+
 }
