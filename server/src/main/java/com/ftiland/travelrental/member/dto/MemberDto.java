a75ed6ce496287ce6196d27477d03989a939790b
ahyoung227 <ahyoung227@gmail.com>
2023-07-10 09:05:49 +0900

    :art: update member controller

--- server/src/main/java/com/ftiland/travelrental/member/dto/MemberDto.java
+++ server/src/main/java/com/ftiland/travelrental/member/dto/MemberDto.java
@@ -1,17 +1,12 @@
 package com.ftiland.travelrental.member.dto;
 
-import com.ftiland.travelrental.category.dto.CategoryDto;
 import com.ftiland.travelrental.member.entity.Member;
-import com.ftiland.travelrental.product.dto.CreateProduct;
-import com.ftiland.travelrental.product.entity.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
-public class GetMember {
+public class MemberDto {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
@@ -27,7 +22,7 @@ public class GetMember {
         private Double latitude;
         private Double longitude;
 
-        public static GetMember.Response from(Member member) {
+        public static MemberDto.Response from(Member member) {
             return Response.builder()
                     .memberId(member.getMemberId())
                     .email(member.getEmail())
