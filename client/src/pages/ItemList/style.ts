dcc0953efb2d129f5b11e91272c04a3ff32f95ed
parkchaeyeon <pcy6904@naver.com>
2023-07-10 11:17:07 +0900

    💄 ItemList Filter SelectBox margin 값 지정

--- client/src/pages/ItemList/style.ts
+++ client/src/pages/ItemList/style.ts
@@ -3,4 +3,6 @@ import styled from 'styled-components';
 export const ItemListPageContainer = styled.div`
   margin-top: 8.375rem;
 `;
-export const ItemFilterWrapper = styled.div``;
+export const ItemFilterWrapper = styled.div`
+  margin-bottom: 2.75rem;
+`;
