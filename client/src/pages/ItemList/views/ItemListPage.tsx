32ebfdf2a119355e33578de868125bab785e7abf
parkchaeyeon <pcy6904@naver.com>
2023-07-10 14:56:50 +0900

    🎨 ItemListPage에서 ItemCard useQuery 사용

--- client/src/pages/ItemList/views/ItemListPage.tsx
+++ client/src/pages/ItemList/views/ItemListPage.tsx
@@ -1,14 +1,45 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useQuery } from 'react-query';
 import SelectBox from '../../../common/components/SelectBox';
-import ItemCardList from '../../../common/components/ItemCard/ItemCardList';
+import ItemCard from '../../../common/components/ItemCard/ItemCard';
 import { ITEMCARD_DATA } from '../../Main/constants';
 import { ItemListPageContainer } from '../style';
-
+import Loading from '../../../common/components/Loading';
 import {
   DISTANCE_DEFAULT_VALUE,
   DISTANCE_OPTIONS,
   PRODUCT_FILTER_OPTIONS,
 } from '../../../common/constants';
+import { ItemCardProps } from '../../../common/type';
+
 function ItemListPage() {
+  const [page, setPage] = useState(1);
+  const [size, setSize] = useState(10);
+  const {
+    data: products,
+    isLoading,
+    error,
+  } = useQuery('products', async () => {
+    try {
+      const res = await axios.get(`/api/products`, {
+        params: {
+          page: page,
+          size: size,
+          category: 'sdkfj234',
+        },
+      });
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <div>에러가 발생했습니다.</div>;
+  }
   return (
     <ItemListPageContainer>
       <div>
@@ -18,7 +49,9 @@ function ItemListPage() {
         />
         <SelectBox selectOptionData={PRODUCT_FILTER_OPTIONS} />
       </div>
-      <ItemCardList itemCardListContentData={ITEMCARD_DATA} />
+      {products.map((product: ItemCardProps) => (
+        <ItemCard key={product.id} itemCardData={product} />
+      ))}
     </ItemListPageContainer>
   );
 }
