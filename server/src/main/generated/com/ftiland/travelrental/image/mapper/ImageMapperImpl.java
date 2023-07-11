9fa3c56e631cbcdbafff2ae98f720953e1b4c0c0
Myungkyulee <skdlaudrb@naver.com>
2023-07-12 02:18:27 +0900

    :sparkles: 더미데이터

---
+++ server/src/main/generated/com/ftiland/travelrental/image/mapper/ImageMapperImpl.java
@@ -0,0 +1,13 @@
+package com.ftiland.travelrental.image.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-07-11T21:41:26+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.17 (Eclipse Adoptium)"
+)
+@Component
+public class ImageMapperImpl implements ImageMapper {
+}
