5224475208ed2bf28a3ecacfedc5cb5befb014e4
jasmine0714 <woooooooow22@gmail.com>
2023-07-12 14:36:54 +0900

    💄 로그인 페이지 컨테이너 생성하여 버튼이 화면 가운데 오도록 구현

--- client/src/pages/Login/style.ts
+++ client/src/pages/Login/style.ts
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
+export const LoginPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
 export const KakaoLoginBtn = styled.div`
   background-image: url('/kakao_login_medium_wide.png');
   background-repeat: no-repeat;
