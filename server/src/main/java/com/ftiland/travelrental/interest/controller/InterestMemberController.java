93c6a47aebae4dd541ab789f632ff5e5ca2547af
minit88 <chymh32@gmail.com>
2023-07-12 11:19:24 +0900

    ✨ 찜하기 컨트롤러 수정

--- server/src/main/java/com/ftiland/travelrental/interest/controller/InterestMemberController.java
+++ server/src/main/java/com/ftiland/travelrental/interest/controller/InterestMemberController.java
@@ -1,11 +1,11 @@
 package com.ftiland.travelrental.interest.controller;
 
+import com.ftiland.travelrental.image.service.ImageService;
 import com.ftiland.travelrental.interest.dto.InterestDto;
 import com.ftiland.travelrental.interest.entity.Interest;
 import com.ftiland.travelrental.interest.mapper.InterestMapper;
 import com.ftiland.travelrental.interest.service.InterestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +20,14 @@ public class InterestMemberController {
 
     private InterestService interestService;
     private InterestMapper interestMapper;
+    private ImageService imageService;
 
     @Autowired
-    public InterestMemberController(InterestService interestService, InterestMapper interestMapper) {
+    public InterestMemberController(InterestService interestService, InterestMapper interestMapper,ImageService imageService) {
+
         this.interestService = interestService;
         this.interestMapper = interestMapper;
+        this.imageService = imageService;
     }
 
 
@@ -33,7 +36,7 @@ public class InterestMemberController {
     public ResponseEntity getInterest(@Param("memberId")Long memberId, @Param("page")@Positive int page, @Param("size")@Positive int size){
 
         ArrayList<Interest> interests = interestService.findInterest(memberId,page,size);
-        InterestDto.ResponsesDto responses = interestMapper.interestsToResponsesDto(interests,page,size);
+        InterestDto.ResponsesDto responses = interestMapper.interestsToResponsesDto(imageService,interests,page,size);
 
         return new ResponseEntity(responses, HttpStatus.OK);
     }
@@ -42,7 +45,7 @@ public class InterestMemberController {
     @PostMapping
     public ResponseEntity postInterest(@Param("memberId") Long memberId, @Param("productId") String productId) {
         Interest interest = interestService.createInterest(memberId, productId);
-        InterestDto.ResponseDto response = interestMapper.interestToResponseDto(interest);
+        InterestDto.PostResponseDto response = interestMapper.interestToPostResponseDto(interest);
 
         return new ResponseEntity(response, HttpStatus.CREATED);
     }
