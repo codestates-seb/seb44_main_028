73ecdb15c76118e89e652e8563f30e65d0fb0496
jasmine0714 <woooooooow22@gmail.com>
2023-07-14 08:24:15 +0900

    ✨ 회원탈퇴 함수 생성 및 onClick 연결

--- client/src/pages/Login/views/LoginPage.tsx
+++ client/src/pages/Login/views/LoginPage.tsx
@@ -1,10 +1,23 @@
+import axios from 'axios';
 import KakaoLogin from '../components/KakaoLogin';
 import { LoginPageContainer } from '../style';
 
 function LoginPage() {
+  const handleWithdrawal = () => {
+    try {
+      console.log('회원탈퇴');
+      const response = axios.delete(
+        process.env.REACT_APP_API_URL + '/api/members',
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <LoginPageContainer>
       <KakaoLogin />
+      <button onClick={handleWithdrawal}>회원탈퇴 임시 버튼</button>
     </LoginPageContainer>
   );
 }
