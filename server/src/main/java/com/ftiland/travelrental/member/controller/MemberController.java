caf0ed4545b09411bc35d35ac3acbc56ebaf5d74
ahyoung227 <ahyoung227@gmail.com>
2023-07-14 22:29:54 +0900

    :sparkles: create memberId method in memberAuthUtils

--- server/src/main/java/com/ftiland/travelrental/member/controller/MemberController.java
+++ server/src/main/java/com/ftiland/travelrental/member/controller/MemberController.java
@@ -1,6 +1,6 @@
 package com.ftiland.travelrental.member.controller;
 
-import com.ftiland.travelrental.common.utils.JwtUtils;
+import com.ftiland.travelrental.common.utils.MemberAuthUtils;
 import com.ftiland.travelrental.member.dto.MemberDto;
 import com.ftiland.travelrental.member.dto.MemberPatchDto;
 import com.ftiland.travelrental.member.entity.Member;
@@ -10,10 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
@@ -29,36 +26,30 @@ public class MemberController {
 
     @GetMapping
     public ResponseEntity<MemberDto.Response> getMember() {
-        Long memberId = (Long) request.getAttribute("memberId");
 
-        if (memberId != null) {
-            Member member = memberService.findMember(memberId);
-            MemberDto.Response response = MemberDto.Response.from(member);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        }
+        Long memberId = MemberAuthUtils.getMemberId(request);
 
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        Member member = memberService.findMember(memberId);
+        MemberDto.Response response = MemberDto.Response.from(member);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PatchMapping
     public ResponseEntity<MemberDto.Response> patchMember(@Valid @RequestBody MemberPatchDto.Request patchRequest) {
-        Long memberId = (Long) request.getAttribute("memberId");
 
-        if (memberId != null) {
-            MemberDto.Response response = memberService.updateMember(patchRequest, memberId);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        Long memberId = MemberAuthUtils.getMemberId(request);
+
+        MemberDto.Response response = memberService.updateMember(patchRequest, memberId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
     }
 
     @DeleteMapping
     public ResponseEntity<Void> deleteMember() {
-        Long memberId = (Long) request.getAttribute("memberId");
 
-        if (memberId != null) {
-            memberService.deleteMember(memberId);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
+        Long memberId = MemberAuthUtils.getMemberId(request);
+
+        memberService.deleteMember(memberId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
