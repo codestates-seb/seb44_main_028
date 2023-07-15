22091263f2456d0bf7a732f1f49f3683ad25d8e5
parkchaeyeon <pcy6904@naver.com>
2023-07-16 04:35:56 +0900

    💄 ItemContent 디테일한 부분 구조 잡기

--- client/src/pages/Detail/components/ItemContent.tsx
+++ client/src/pages/Detail/components/ItemContent.tsx
@@ -1,12 +1,36 @@
-import { ItemContentContainer } from '../style';
+import {
+  ItemContentContainer,
+  ItemInfoWrapper,
+  ItemImageWrapper,
+  ItemUserWrapper,
+  ItemDescriptionWrapper,
+  ItemActionBtn,
+  ProductInfo,
+  ProductTitle,
+  ProductContent,
+  ProductBtn,
+} from '../style';
 const ItemContent = () => {
   return (
     <ItemContentContainer>
       <ItemInfoWrapper>
-        <ItemImageWrapper></ItemImageWrapper>
-        <ItemUserWrapper></ItemUserWrapper>
+        <ItemImageWrapper>
+          <img src="http://www.arpheoworks.com/wp-content/uploads/2015/04/tacotruck_lr3.jpg" />
+        </ItemImageWrapper>
+        <ItemUserWrapper>
+          {/* 유저 정보 */}
+          {/* 가격 정보 */}
+          {/* 별점 */}
+          <ItemActionBtn></ItemActionBtn>
+        </ItemUserWrapper>
       </ItemInfoWrapper>
-      <ItemDescriptionWrapper></ItemDescriptionWrapper>
+      <ItemDescriptionWrapper>
+        <ProductInfo></ProductInfo>
+        <ProductTitle></ProductTitle>
+        <ProductContent></ProductContent>
+        {/* 카테고리 */}
+        <ProductBtn></ProductBtn>
+      </ItemDescriptionWrapper>
     </ItemContentContainer>
   );
 };
--- client/src/pages/Detail/style.ts
+++ client/src/pages/Detail/style.ts
@@ -3,8 +3,20 @@ export const ItemContentContainer = styled.div`
   display: flex;
   flex-direction: column;
 `;
-export const ItemInfoWrapper = styled.div``;
-export const ItemImageWrapper = styled.div``;
-export const ItemUserWrapper = styled.div``;
-
+export const ItemInfoWrapper = styled.div`
+  height: 21.688rem;
+  width: 100%;
+`;
+export const ItemImageWrapper = styled.div`
+  width: 22.5rem;
+  height: 100%;
+`;
+export const ItemUserWrapper = styled.div`
+  background-color: gray;
+`;
+export const ItemActionBtn = styled.div``;
 export const ItemDescriptionWrapper = styled.div``;
+export const ProductInfo = styled.div``;
+export const ProductTitle = styled.div``;
+export const ProductContent = styled.div``;
+export const ProductBtn = styled.div``;
