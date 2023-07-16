b18ef0c09baff8e90334c909561279ad4909ebb1
jasmine0714 <woooooooow22@gmail.com>
2023-07-16 21:30:06 +0900

    🔥 각 컴포넌트 내부에서 메시지를 subscribe 함에 따라 prop을 넘기지 않음

--- client/src/pages/Chatting/views/ChatRoomArea.tsx
+++ client/src/pages/Chatting/views/ChatRoomArea.tsx
@@ -4,11 +4,10 @@ import ChattingMessages from '../components/ChattingMessages';
 import { ChatRoomAreaWrapper } from '../style';
 
 function ChatRoomArea() {
-  const [newMessage, setNewMessage] = useState<string>('');
   return (
     <ChatRoomAreaWrapper>
       <ChattingMessages />
-      <ChattingInput newMessage={newMessage} setNewMessage={setNewMessage} />
+      <ChattingInput />
     </ChatRoomAreaWrapper>
   );
 }
