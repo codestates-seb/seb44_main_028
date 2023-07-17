9844e69d694c02dba352b71482e1d9c7b9cdda1e
parkchaeyeon <pcy6904@naver.com>
2023-07-17 14:17:40 +0900

    ⚡️ 💄 UpdatePage 구조, 상품 정보 get

--- client/src/pages/Update/views/UpdatePage.tsx
+++ client/src/pages/Update/views/UpdatePage.tsx
@@ -1,4 +1,20 @@
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import WritePost from '../../Create/components/WritePost';
 function UpdatePage() {
-  return <div>update page</div>;
+  const param = useParams();
+  const { data, isLoading, error } = useQuery('updateDetail', async () => {
+    const { data } = await axios.get(
+      `${process.env.REACT_APP_API_URL}/api/products/${param.itemId}`,
+    );
+    console.log(data);
+    return data;
+  });
+  return (
+    <div>
+      <WritePost />
+    </div>
+  );
 }
 export default UpdatePage;
