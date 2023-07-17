e8b830f611357eee495d5b69c9f534148da5d551
minit88 <chymh32@gmail.com>
2023-07-17 16:10:05 +0900

    :sparkle: DTO, 예외항목 추가

--- server/src/main/java/com/ftiland/travelrental/chat/service/ChatEntityService.java
+++ server/src/main/java/com/ftiland/travelrental/chat/service/ChatEntityService.java
@@ -1,5 +1,6 @@
 package com.ftiland.travelrental.chat.service;
 
+import com.ftiland.travelrental.chat.dto.ResponseDto;
 import com.ftiland.travelrental.chat.entity.ChatMessage;
 import com.ftiland.travelrental.chat.entity.ChatRoom;
 import com.ftiland.travelrental.chat.entity.ChatRoomMembers;
@@ -8,13 +9,13 @@ import com.ftiland.travelrental.chat.repository.ChatRoomMembersRepository;
 import com.ftiland.travelrental.chat.repository.ChatMessageRepository;
 
 import com.ftiland.travelrental.chat.repository.ChatRoomRepository;
+import com.ftiland.travelrental.common.exception.BusinessLogicException;
+import com.ftiland.travelrental.common.exception.ExceptionCode;
 import com.ftiland.travelrental.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -82,8 +83,8 @@ public class ChatEntityService {
         return messages;
     }
 
-    public List<String> findChatRoom(Long memberId){
-        List<String> rooms = chatRoomRepository.findChatRoomsByUserId(memberId);
+    public List<ChatRoom> findChatRoom(Long memberId){
+        List<ChatRoom> rooms = chatRoomRepository.findChatRoomsByUserId(memberId);
 
         return rooms;
     }
@@ -92,5 +93,12 @@ public class ChatEntityService {
         List<ChatMessage> messages = chatMessageRepository.findByMemberId(memberId);
 
         return messages;
+    }
+
+    public BusinessLogicException findChatRoom(Long memberId1, Long memberId2){
+        if(chatRoomRepository.findChatRoomsWithMembers(memberId1,memberId2).isPresent()){
+            return new BusinessLogicException(ExceptionCode.CHATROOM_ALREADY_EXISTS);
+        }
+        return null;
     }
 }
