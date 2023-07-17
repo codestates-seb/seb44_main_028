290dd753c0163ed18bbeed4cbe767ca42a302cbf
parkchaeyeon <pcy6904@naver.com>
2023-07-18 00:44:38 +0900

    🎨 검색 라우터 경로 추가

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -9,6 +9,7 @@ import CreatePage from './pages/Create/views/CreatePage';
 import UpdatePage from './pages/Update/views/UpdatePage';
 import ChattingPage from './pages/Chatting/views/ChattingPage';
 import BookingPage from './pages/Booking/views/BookingPage';
+import SearchPage from './pages/Search/views/SearchPage';
 import PrivateRoutes from './PrivateRoutes';
 import ProfileEdit from './pages/MyPage/components/ProfileEdit';
 
@@ -19,6 +20,7 @@ function Router() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/auth/callback" element={<RedirectPage />} />
       <Route path="/itemlist/:categoryId" element={<ItemListPage />} />
+      <Route path="/itemlist/search/:serachContent" element={<SearchPage />} />
       {/* <Route element={<PrivateRoutes />}> */}
       <Route path="/booking/:itemId" element={<BookingPage />} />
       <Route path="/mypage" element={<MyPage />} />
