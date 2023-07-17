1d9223b365660f58c83ef14033c45f069396a6bf
parkchaeyeon <pcy6904@naver.com>
2023-07-18 01:27:35 +0900

    💄 SearchPage 구조 잡기

---
+++ client/src/pages/Search/style.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+export const SearchPageContainer = styled.div`
+  margin-top: 5.375rem;
+`;
+export const SearchProductListWrapper = styled.div``;
--- client/src/pages/Search/views/SearchPage.tsx
+++ client/src/pages/Search/views/SearchPage.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
-
+import SelectBox from '../../../common/components/SelectBox';
+import {
+  DISTANCE_DEFAULT_VALUE,
+  DISTANCE_OPTIONS,
+  PRODUCT_FILTER_OPTIONS,
+} from '../../../common/constants';
+import { SearchPageContainer, SearchProductListWrapper } from '../style';
 const SearchPage = () => {
-  return <div></div>;
+  return (
+    <SearchPageContainer>
+      <div>
+        <SelectBox
+          selectOptionData={DISTANCE_OPTIONS}
+          selectDefaultOption={DISTANCE_DEFAULT_VALUE}
+        />
+        <SelectBox selectOptionData={PRODUCT_FILTER_OPTIONS} />
+      </div>
+      <SearchProductListWrapper></SearchProductListWrapper>
+    </SearchPageContainer>
+  );
 };
 
 export default SearchPage;
