965cf9d70054a084c109d01d30312ec602740220
parkchaeyeon <pcy6904@naver.com>
2023-07-18 01:48:01 +0900

    ⚡️ 검색한 결과 itemCard 컴포넌트로 매핑하여 띄우기

--- client/src/pages/Search/views/SearchPage.tsx
+++ client/src/pages/Search/views/SearchPage.tsx
@@ -1,4 +1,10 @@
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
 import SelectBox from '../../../common/components/SelectBox';
+import ItemCard from '../../../common/components/ItemCard/ItemCard';
+import { ItemCardProps } from '../../../common/type';
 import {
   DISTANCE_DEFAULT_VALUE,
   DISTANCE_OPTIONS,
@@ -6,6 +12,28 @@ import {
 } from '../../../common/constants';
 import { SearchPageContainer, SearchProductListWrapper } from '../style';
 const SearchPage = () => {
+  const params = useParams();
+  const [page, setPage] = useState(0);
+  const size = 10;
+  const { data, isLoading, error } = useQuery('searchProduct', async () => {
+    try {
+      const res = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/products/search`,
+        {
+          params: {
+            keyword: params.searchContent,
+            page: page,
+            size: size,
+          },
+        },
+      );
+      console.log('res', res);
+      return res.data;
+    } catch (err) {
+      console.log('err', err);
+    }
+  });
+
   return (
     <SearchPageContainer>
       <div>
@@ -15,7 +43,11 @@ const SearchPage = () => {
         />
         <SelectBox selectOptionData={PRODUCT_FILTER_OPTIONS} />
       </div>
-      <SearchProductListWrapper></SearchProductListWrapper>
+      <SearchProductListWrapper>
+        {data?.products.map((item: ItemCardProps) => (
+          <ItemCard key={item.productId} itemCardData={item} />
+        ))}
+      </SearchProductListWrapper>
     </SearchPageContainer>
   );
 };
