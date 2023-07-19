c0f15dc5d1dcb08bb04cce473ecc34b57f151e2d
parkchaeyeon <pcy6904@naver.com>
2023-07-19 22:29:03 +0900

    ✨ 무한스크롤 구현

--- client/src/pages/ItemList/views/ItemListPage.tsx
+++ client/src/pages/ItemList/views/ItemListPage.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import SelectBox from '../../../common/components/SelectBox';
 import ItemCard from '../../../common/components/ItemCard/ItemCard';
-import { ItemListPageContainer, ProductListWrapper } from '../style';
 import Loading from '../../../common/components/Loading';
 import {
   DISTANCE_DEFAULT_VALUE,
@@ -11,19 +11,22 @@ import {
   PRODUCT_FILTER_OPTIONS,
 } from '../../../common/constants';
 import { ItemCardProps } from '../../../common/type';
-import { useParams } from 'react-router-dom';
 import NoData from '../../../common/components/NoData';
+import ErrorPage from '../../../common/components/ErrorPage';
+import { ItemListPageContainer, ProductListWrapper } from '../style';
 
 function ItemListPage() {
   const params = useParams();
   const [page, setPage] = useState(1);
-  console.log(params);
-  const size = 10;
+  const size = 3;
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [items, setItems] = useState<ItemCardProps[]>([]);
   const {
     data: products,
     isLoading,
     error,
-  } = useQuery('products', async () => {
+    isFetching,
+  } = useQuery(['products', page], async () => {
     try {
       const res = await axios.get(
         `${process.env.REACT_APP_API_URL}/api/products`,
@@ -35,19 +38,41 @@ function ItemListPage() {
           },
         },
       );
-      console.log('res', res);
-      return res.data;
+      setItems((prevIetm) => [...prevIetm, ...res.data.products]);
+      return res.data.products;
     } catch (err) {
       console.log('err', err);
     }
   });
-  if (isLoading) {
+
+  useEffect(() => {
+    function handleScroll() {
+      if (
+        containerRef.current &&
+        containerRef.current.getBoundingClientRect().bottom <=
+          window.innerHeight
+      ) {
+        if (!isFetching) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isFetching]);
+
+  if (isLoading && page === 1) {
     return <Loading />;
   }
+
   if (error) {
-    return <div>에러가 발생했습니다.</div>;
+    return <ErrorPage />;
   }
-  if (products.products.length === 0) {
+
+  if (!isLoading && items?.length === 0) {
     return <NoData />;
   }
   console.log(products);
@@ -60,12 +85,14 @@ function ItemListPage() {
         />
         <SelectBox selectOptionData={PRODUCT_FILTER_OPTIONS} />
       </div>
-      <ProductListWrapper>
-        {products?.products.map((product: ItemCardProps) => (
+      <ProductListWrapper ref={containerRef}>
+        {items?.map((product: ItemCardProps) => (
           <ItemCard key={product.productId} itemCardData={product} />
         ))}
       </ProductListWrapper>
+      {isFetching && <Loading />}
     </ItemListPageContainer>
   );
 }
+
 export default ItemListPage;
