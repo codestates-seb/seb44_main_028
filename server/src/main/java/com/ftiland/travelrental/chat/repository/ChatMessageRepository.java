df66e125d8da65d12ffedf1e07d9c6dbf4303ef4
minit88 <chymh32@gmail.com>
2023-07-21 09:52:20 +0900

    :sparkles: 채팅방 접속 시 제공할 채팅방 리스트 수정

--- server/src/main/java/com/ftiland/travelrental/chat/repository/ChatMessageRepository.java
+++ server/src/main/java/com/ftiland/travelrental/chat/repository/ChatMessageRepository.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage,String> {
 
+    @Query("SELECT cm FROM ChatMessage cm WHERE cm.chatRoom.chatroomId =: roomId")
+    List<ChatMessage> findByRoomId(@Param("roomId") String roomId);
 
-    List<ChatMessage> findByRoomId(String roomId);
-
-    @Query("SELECT cm FROM ChatMessage cm JOIN ChatRoomMembers crm ON cm.roomId = crm.chatroom.chatroomId WHERE crm.member.memberId = :memberId ")
+    @Query("SELECT cm FROM ChatMessage cm JOIN ChatRoomMembers crm ON cm.chatRoom.chatroomId = crm.chatroom.chatroomId WHERE crm.member.memberId = :memberId ")
     List<ChatMessage> findByMemberId(@Param("memberId") long memberId);
 }
