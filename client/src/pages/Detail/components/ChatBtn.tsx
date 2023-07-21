15e5606fb727ee2027fd20eeb1c80003d8146164
jasmine0714 <woooooooow22@gmail.com>
2023-07-21 23:21:06 +0900

    ⚡️ React-Query를 사용하여 기존 채팅방 존재 여부와

--- client/src/pages/Detail/components/ChatBtn.tsx
+++ client/src/pages/Detail/components/ChatBtn.tsx
@@ -5,84 +5,155 @@ import useGetMe from '../../../common/utils/customHooks/useGetMe';
 import BigDefaultBtn from '../../../common/components/Button';
 import { colorPalette } from '../../../common/utils/enum/colorPalette';
 import { ACCESS_TOKEN } from '../../Login/constants';
+import { useMutation, useQuery } from 'react-query';
+import Loading from '../../../common/components/Loading';
+import ErrorPage from '../../../common/components/ErrorPage';
 
 function makeChatRoomName(senderId: number, receiverId: number) {
   const chatRoomName = [String(senderId), String(receiverId)].sort().join('');
   return chatRoomName;
 }
 
+const fetchChatRoomExistsOrNot = async (
+  productId: string | undefined,
+  accessToken: string,
+) => {
+  const response = await axios.get(
+    `${process.env.REACT_APP_API_URL}/api/chat/seller?productId=${productId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  );
+  return response.data;
+};
+
+const fetchChatRoomId = async (
+  senderId: number,
+  itemId: string | undefined,
+  accessToken: string,
+) => {
+  try {
+    // 채팅방 생성 요청을 보낸다.
+    // if (!chatRoomExistsOrNot) {
+    //   const postResponse = await axios.post(
+    //     process.env.REACT_APP_API_URL + '/api/chat',
+    //     {
+    //       senderId,
+    //       receiverId,
+    //       name: makeChatRoomName(senderId, receiverId),
+    //     },
+    //   );
+    //   console.log('채팅방 생성 결과', postResponse);
+    // }
+
+    // 채팅방 아이디를 가져온다.
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_URL}/api/chat/chatroom`,
+      {
+        params: {
+          senderId: senderId,
+          productId: itemId,
+        },
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+    console.log('채팅방 생성 결과', response.data);
+    const chatRoomId = response.data.chatroomId;
+    console.log('채팅방 아이디', chatRoomId);
+    return chatRoomId;
+  } catch (error) {
+    console.error('Failed to fetch chat room:', error);
+    throw error;
+  }
+};
+
 function ChatBtn() {
+  // 1. productId를 가져온다.
   const { itemId } = useParams<{ itemId: string }>();
   const navigate = useNavigate();
-  const [receiverId, setReceiverId] = useState<number>(0);
-  const [hasChattingRecordWithReceiver, setHasChattingRecordWithReceiver] =
-    useState<boolean>(false);
+  // const [receiverId, setReceiverId] = useState<number>(0);
+  // const [chatRoomExistsOrNot, setHasChatRoomExists] = useState<boolean>(false);
   const encryptedAccessToken = localStorage.getItem(ACCESS_TOKEN);
   const accessToken = encryptedAccessToken || '';
 
   // 판매자의 memberId를 가져온다.
-  useEffect(() => {
-    const fetchReceiverId = async () => {
-      try {
-        const response = await axios.get(
-          process.env.REACT_APP_API_URL +
-            `/api/chat/seller?productId=${itemId}`,
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          },
-        );
-        console.log('판매자 정보', response.data);
-        console.log('판매자의 memberId', response.data.sellerId);
-        // 컴포넌트가 아직 마운트된 상태라면 상태를 설정
-        setReceiverId(response.data.sellerId);
-        setHasChattingRecordWithReceiver(response.data.hasChattingRecord);
-      } catch (error) {
-        console.error('Failed to fetch seller:', error);
-      }
-    };
-    fetchReceiverId();
-  }, []);
+  // useEffect(() => {
+  //   const fetchReceiverId = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         process.env.REACT_APP_API_URL +
+  //           `/api/chat/seller?productId=${itemId}`,
+  //         {
+  //           headers: {
+  //             Authorization: `Bearer ${accessToken}`,
+  //           },
+  //         },
+  //       );
+  //       console.log('판매자 정보', response.data);
+  //       // 컴포넌트가 아직 마운트된 상태라면 상태를 설정
+  //       // setReceiverId(response.data.sellerId);
+  //       // 2. 판매자의 productId를 이용해 채팅방이 존재하는지 확인한다.
+  //       setHasChattingRecordWithReceiver(response.data.chatRoomExistsOrNot);
+  //     } catch (error) {
+  //       console.error('Failed to fetch seller:', error);
+  //     }
+  //   };
+  //   fetchReceiverId();
+  // }, []);
+  const {
+    data: chatRoomExistsOrNot,
+    isLoading,
+    error,
+    isError,
+  } = useQuery(
+    ['chatRoomExistsOrNot', itemId],
+    () => fetchChatRoomExistsOrNot(itemId, accessToken),
+    {
+      retry: 1,
+      refetchOnWindowFocus: true,
+      staleTime: 5 * 1000,
+      cacheTime: 30 * 1000,
+    },
+  );
+
+  // boolean 값
+  console.log('채팅방 존재 여부', chatRoomExistsOrNot);
 
   const { data: userData } = useGetMe();
   if (!userData) {
     navigate('/login');
     return null;
   }
+  // 3. senderId 아이디를 가져온다.
   const senderId = userData.memberId;
 
   console.log('내 아이디', senderId);
 
-  // 판매자의 memberId를 이용해 채팅방을 생성한다. TODO: 추후 custom hook으로 분리할 예정
-  const createChatRoom = async () => {
-    try {
-      // 채팅방 생성 요청을 보낸다.
-      if (!hasChattingRecordWithReceiver) {
-        const postResponse = await axios.post(
-          process.env.REACT_APP_API_URL + '/api/chat',
-          {
-            senderId,
-            receiverId,
-            name: makeChatRoomName(senderId, receiverId),
-          },
-        );
-        console.log('채팅방 생성 결과', postResponse);
-      }
+  const createChatRoomMutation = useMutation(
+    () => fetchChatRoomId(senderId, itemId, accessToken),
+    {
+      onSuccess: (chatRoomId) => {
+        console.log('채팅방 아이디', chatRoomId);
+      },
+      onError: (error) => {
+        console.error('Failed to create chat room:', error);
+        // TODO: 상태코드에 따른 에러 처리
+      },
+    },
+  );
 
-      // 채팅방 아이디를 가져온다.
-      const getResponse = await axios.get(
-        process.env.REACT_APP_API_URL +
-          `/api/chat/chatroom?senderId=${senderId}&receiverId=${receiverId}`,
-      );
-      console.log('채팅방 생성 결과', getResponse.data);
-      const chatRoomId = getResponse.data.chatroomId;
-      console.log('채팅방 아이디', chatRoomId);
-      // post 요청을 보낸 후, 채팅방으로 이동한다.
-      navigate(`/chatting/${chatRoomId}`);
-    } catch (error) {
-      console.error('Failed to create chat room:', error);
-      // TODO: 상태코드에 따른 에러 처리
+  const handleNavigateToChatRoom = async () => {
+    if (chatRoomExistsOrNot) {
+      // 채팅방이 이미 있으면 채팅방 Id를 GET 요청으로 받아서 채팅 페이지로 이동
+      const { data: chatRoomId } = await createChatRoomMutation.mutateAsync();
+      navigate(`/chatting/${itemId}/${chatRoomId}`);
+    } else {
+      // 채팅방이 일단 채팅방으로 이동
+      navigate(`/chatting/${itemId}`);
     }
   };
 
@@ -93,7 +164,7 @@ function ChatBtn() {
       hoverBackgroundColor={colorPalette.modalCancelHoverColor}
       height={64}
       width={100}
-      onClick={createChatRoom}
+      onClick={handleNavigateToChatRoom}
     >
       채팅하기
     </BigDefaultBtn>
