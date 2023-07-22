e293233a2d8341fe76fd3e3d7c82868c71cea68b
parkchaeyeon <pcy6904@naver.com>
2023-07-23 07:03:11 +0900

    ✨ 🐛  🔥 찜하기 구현 / 불필요한 코드, 파일 삭제

--- client/src/common/store/RootStore.tsx
+++ client/src/common/store/RootStore.tsx
@@ -2,7 +2,6 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { userInfo } from './UserInfoStore';
 import { calendar } from '../../pages/Booking/store/CalendarStore';
 import { reservation } from '../../pages/Booking/store/ReservationDateStore';
-import { interestProducts } from './InterestStore';
 import { monthlyReservation } from '../../pages/Booking/store/MonthlyReservationStore';
 import { mypageProfileSlice } from '../../pages/MyPage/store/ProfileSlice';
 
@@ -10,7 +9,6 @@ export const rootReducer = combineReducers({
   userInfo: userInfo.reducer,
   calendar: calendar.reducer,
   reservation: reservation.reducer,
-  interestProducts: interestProducts.reducer,
   monthlyReservation: monthlyReservation.reducer,
   mypageProfileSlice: mypageProfileSlice.reducer,
 });
