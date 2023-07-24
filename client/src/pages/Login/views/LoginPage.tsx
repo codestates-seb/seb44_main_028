5f64cb68b5b3b4f4ee344538d12abb80254494d4
jasmine0714 <woooooooow22@gmail.com>
2023-07-24 22:33:51 +0900

    🔥 로고 일시 주석 처리

--- client/src/pages/Login/views/LoginPage.tsx
+++ client/src/pages/Login/views/LoginPage.tsx
@@ -4,32 +4,19 @@ import useDecryptToken from '../../../common/utils/customHooks/useDecryptToken';
 import KakaoLogin from '../components/KakaoLogin';
 import { LoginPageContainer } from '../style';
 import { ACCESS_TOKEN } from '../constants';
+import styled from 'styled-components';
+
+const Logo = styled.img`
+  width: 500px;
+  margin-right: 170px;
+  clip-path: inset(0 0 0 35%);
+`;
 
 function LoginPage() {
-  const navigate = useNavigate();
-  const decrypt = useDecryptToken();
-  const encryptedAccessToken = localStorage.getItem(ACCESS_TOKEN) || '';
-  const accessToken = decrypt(encryptedAccessToken);
-  const handleWithdrawal = async () => {
-    try {
-      await axios.delete(process.env.REACT_APP_API_URL + '/api/members', {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      localStorage.removeItem(ACCESS_TOKEN);
-      navigate('/');
-    } catch (error: AxiosError | any) {
-      if (error.response.status === 401) {
-        alert('로그인이 만료되었습니다. 다시 로그인해주세요');
-      }
-      navigate('/login');
-    }
-  };
   return (
     <LoginPageContainer>
+      {/* <Logo src="/logo.png" /> */}
       <KakaoLogin />
-      <button onClick={handleWithdrawal}>회원탈퇴 임시 버튼</button>
     </LoginPageContainer>
   );
 }
