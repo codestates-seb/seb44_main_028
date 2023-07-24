e315cf21311e546ae6a8ea1b06b99d5ebc193bda
jasmine0714 <woooooooow22@gmail.com>
2023-07-24 23:42:18 +0900

    🔥 로고 주석 처리

--- client/src/pages/Login/views/LoginPage.tsx
+++ client/src/pages/Login/views/LoginPage.tsx
@@ -3,19 +3,19 @@ import axios, { AxiosError } from 'axios';
 import useDecryptToken from '../../../common/utils/customHooks/useDecryptToken';
 import KakaoLogin from '../components/KakaoLogin';
 import { LoginPageContainer } from '../style';
-import { ACCESS_TOKEN } from '../constants';
 import styled from 'styled-components';
 
-const Logo = styled.img`
+const Logo = styled.div`
   width: 500px;
   margin-right: 170px;
   clip-path: inset(0 0 0 35%);
+  background-image: url('/public/logo.png');
 `;
 
 function LoginPage() {
   return (
     <LoginPageContainer>
-      {/* <Logo src="/logo.png" /> */}
+      <Logo />
       <KakaoLogin />
     </LoginPageContainer>
   );
